279d47f4d68f083b9c75aeebcb83002e13819cb7
lipemizani <filipe.mizani99@gmail.com>
2024-03-25 17:04:08 -0300

    Atividade 4.1

---
+++ AtividadeQuatroPontoUm/src/galToLit/GalToLit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package galToLit;
+
+/**
+ * 
+ */
+public class GalToLit {
+
+	public static void main(String[] args) {
+		
+		double gallons=10;
+		double liters=0;
+		
+		liters = gallons * 3.785;
+		
+		System.out.println(gallons + " Galões equivalem a " + liters + " litros ");
+	}
+}
